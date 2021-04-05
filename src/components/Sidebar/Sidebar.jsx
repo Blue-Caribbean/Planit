7f59ef05c3d660ca338d3c9180f775888c7a54b0
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-04-05 16:22:21 -0700

    Implemented basic skeleton of sidebar. Has full onClick functionality in order to pass props up to parent

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,24 +10,46 @@ class Sidebar extends React.Component {
         friends: ['Charlie', 'John', 'Ted', 'Joe', 'HoneyBooBoo'],
         groups: ['Galactus Maximus', 'Fathers who Fart', 'Fidget Spinners Anonomous', 'Wall-Mart'],
       },
+      groupsSelected: true,
     };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(event) {
+    const { groupsSelected } = this.state;
+    if (groupsSelected === true) {
+      this.setState({
+        groupsSelected: false,
+      });
+    } else {
+      this.setState({
+        groupsSelected: true,
+      });
+    }
   }
 
   render() {
-    /*
-      Sidebar component
-      1. Friends button
-      2. Groups button
-      3. Plus button
-      4. list
-    */
-    const { dummy } = this.state;
+    const { dummy, groupsSelected } = this.state;
+    let friendsColor = 'red';
+    let groupsColor = 'white';
+    if (groupsSelected === true) {
+      friendsColor = 'white';
+      groupsColor = 'red';
+    }
     return (
       <div id="sidebarContainer">
-        <button type="button">Friends</button>
-        <button type="button">Groups</button>
+        <button type="button" onClick={this.handleToggle} color={groupsColor}>
+          Groups
+        </button>
+        <button type="button" id="friendsButton" onClick={this.handleToggle} color={friendsColor}>
+          Friends
+        </button>
         <button type="button">+</button>
-        <SidebarList friends={dummy.friends} groups={dummy.groups} />
+        <SidebarList
+          friends={dummy.friends}
+          groups={dummy.groups}
+          isGroupsSelected={groupsSelected}
+        />
       </div>
     );
   }
--- src/components/Sidebar/SidebarList.jsx
+++ src/components/Sidebar/SidebarList.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
 
-const SidebarList = (props) => {
-  const { friends, groups } = props;
+class SidebarList extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="SidebarList">
-      <ul />
-    </div>
-  );
-};
+    this.state = {
+      clicked: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      clicked: event.target.value,
+    });
+  }
+
+  render(props) {
+    const { friends, groups, isGroupsSelected } = this.props;
+
+    if (isGroupsSelected === false) {
+      return (
+        <div className="sidebarList">
+          <ul>
+            {friends.map((friend, index) => (
+              <li key={index} onClick={this.handleClick}>
+                {friend}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    return (
+      <div className="sidebarList">
+        <ul>
+          {groups.map((group, index) => (
+            <li key={index} onClick={this.handleClick}>
+              {group}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SidebarList;
