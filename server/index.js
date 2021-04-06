75751ea87c84c073e482f7cfb8a6530f688fabc1
Ross Russell <ross.russell@protonmail.com>
2021-04-05 17:00:21 -0700

    starting on the endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
+app.get('/api/:userid/groups', (req, res) => {
+  // get all groups for this user id and return them.
+  res.status(500).send('Fatal no data.');
+});
+
+app.get('/api/:userid/friends', (req, res) => {
+  // get all friends.
+  res.status(500).send('Fatal no data.');
+});
+
 app.listen(port, () => {
-  console.log("express is on port " + port);
+  console.log('express is on port ' + port);
 });
